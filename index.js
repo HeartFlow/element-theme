dea2d55b0e4e789b31de1bf4fcfeb0b01d9aaa69
Philippe Eng <peng@heartflow.com>
2019-07-16 14:50:19 -0500

    Trigger a full rebuild on watch changes

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ exports.init = function (filePath) {
 }
 
 exports.watch = function (opts) {
-  gulp.task('build', build(opts))
   exports.run(opts)
-  gulp.watch(config.watchGlobs || config.config, ['build'])
+  gulp.watch(config.watchGlobs || config.config, function() {
+    exports.run(opts)
+  })
 }
 
 exports.run = function (opts, cb) {
